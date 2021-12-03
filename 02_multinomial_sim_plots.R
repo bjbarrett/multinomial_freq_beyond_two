2ed23420aed6cf7410c66ca4cd4688133d89e3fc
Brendan Barrett <bbarrett@ab.mpg.de>
2021-12-03 14:36:26 +0100

    sims of 30 at f=3, 10,20,50,100,200,5000

--- 02_multinomial_sim_plots.R
+++ 02_multinomial_sim_plots.R
@@ -1,7 +1,9 @@
 library(rethinking)
 library(RColorBrewer)
 knitr::opts_chunk$set(echo = TRUE)
-master2 <- read.csv("conform_sims_fis3_log.csv") #load data
+master <- read.csv("f_3_n_10_20_50_nsims_30_fits.csv") #load data
+master1 <- read.csv("f_3_n_100_200_500_nsims_30_fits.csv") #load data
+master2 <- rbind(master,master1)
 master2 <- master2[,-1] # get rid of bs first column
 nsims <- ncol(master2) - 3 #number of sims 
 
@@ -12,32 +14,33 @@ mypalette<-brewer.pal(max(K),"Dark2")
 
 plot.new()
 par( mfrow = c( length(N) , length(K) ) ) 
-par(mar=c(2.5,0,1,0)+.1)
+par(mar=c(0,0,0,0)+.1)
 for (f_i in 1:length(F)){
   for (n_i in 1:length(N)){
     for (k_i in 1:length(K)){
       z <- master2[master2$k==K[k_i],] #subset relevant k
       zm <- data.matrix(z[,1:nsims]) #extract parameter estimates
       ##plots
+      ylimz <- 3*max(density(zm)$y)
       index <- which(z$n==N[n_i]) #get rows for relevant n in loop
       textz <- paste("k =",K[k_i],"; n =",N[n_i],"; f =",F[f_i] )   #title of plots
-      dens( zm[index,] , show.HPDI=0.9999 , col=mypalette[k_i] , main=textz ,
-            xlim=c(0,8) ,ylim=c(0,1) , cex.main=0.8 , xlab='' , yaxt='n') #post of all sims
-      curve(dlnorm(x, meanlog=1, sdlog=1), from=0, to=10 , add=TRUE , lty=3 , col=1) #plot prior
-      dens(exp(rnorm(1e7 , mean=0 , sd=1)) , lty=2 , add=TRUE)
+      dens( zm[index,] , col=mypalette[k_i] , main=textz ,
+            xlim=c(0,8) ,ylim=c(0,1.2) , cex.main=0.8 , xlab='' , yaxt='n' , xaxt='n') #post of all sims
+      curve(dlnorm(x, meanlog=0, sdlog=1), from=0, to=10 , add=TRUE , lty=3 , col=1 ) #plot prior
+      #dens(exp(rnorm(1e7 , mean=0 , sd=1)) , lty=2 , add=TRUE)
       abline(v=F[f_i]) #line at true value
       seq_l <- seq(from=0 , to=0.35 , length=nsims) #vertical range to plot hpdi segs
       #f_med_order <- order(apply(zm[index,] , 2 , median)) #order of model f medians by magnitude
       f_med <- apply(zm[index,] , 2 ,  median) #actual medians
       f_hpdi <- apply(zm[index,] , 2 ,  HPDI) #actual medians
-      
-      points(f_med[order(f_med)] , seq_l , cex=0.3)
       for (i in 1:nsims) dens(zm[index,i] , add=TRUE ,  col=col.alpha(mypalette[k_i] , alpha=0.15))
-      
       for(i in 1:nsims){
         segments( x0=f_hpdi[,order(f_med)[i]][1] , y0=seq_l[i] , 
-                  x1=f_hpdi[,order(f_med)[i]][2], y1=seq_l[i] , prob=0.8)
+                  x1=f_hpdi[,order(f_med)[i]][2], y1=seq_l[i] , prob=0.5 , col.alpha("darkgrey" , alpha=1) , lty=1 , lw=.5)
       }
+      points(f_med[order(f_med)] , seq_l , cex=0.3 ,  col=mypalette[k_i] , alpha=0.99 , pch=19)
+      
+      
     }
   }
 }
