4b980a11d27f28cabf1f18fb47c0d50349c0943e
Brendan Barrett <bbarrett@ab.mpg.de>
2021-12-16 17:54:04 +0100

    some changes

--- 02_multinomial_sim_plots.R
+++ 02_multinomial_sim_plots.R
@@ -16,7 +16,7 @@ nsims <- ncol(master2) - 3 #number of sims
 N <- sort(unique(master2$n))  ## pop size vector from data
 F <-sort(unique(master2$f))  ## strength of frequency dependence from data
 K <- sort(unique(master2$k))  ## number of options from data
-mypalette<-brewer.pal(max(K),"Dark2")
+mypalette<-brewer.pal(max(K),"BuPu")
 prior_dist <- exp(rnorm(3000, mean=0, sd=1 ))
 plot.new()
 par( mfrow = c( length(N) , length(K) ) ) 
@@ -32,7 +32,7 @@ for (f_i in 1:length(F)){
 
       index <- which(z$n==N[n_i]) #get rows for relevant n in loop
       textz <- paste("k =",K[k_i],"; n =",N[n_i],"; f =",F[f_i] )   #title of plots
-      dens( zm[index,] , col=mypalette[k_i] , xlim=c(0,7) ,ylim=c(0,3.5) , xlab='',ylab='' , xaxt='n' , yaxt='n') #post of all sims
+      dens( zm[index,] , col=mypalette[k_i] , xlim=c(0,7) ,ylim=c(0,3.5) , xlab='',ylab='' , xaxt='n' , yaxt='n' , adj=2) #post of all sims
       dens(prior_dist, from=0, to=10 , add=TRUE , lty=3 , col=1 ) #plot prior
       title(main=textz , line=-.75 , cex.main=0.8)
       #dens(exp(rnorm(1e7 , mean=0 , sd=1)) , lty=2 , add=TRUE)
@@ -42,10 +42,10 @@ for (f_i in 1:length(F)){
       f_med <- apply(zm[index,] , 2 ,  median) #actual medians
       f_hpdi <- apply(zm[index,] , 2 , HPDI , prob=0.50) #actual medians
       for (i in 1:nsims) dens(zm[index,i] , add=TRUE ,  col=col.alpha(mypalette[k_i] , alpha=0.05))
-      # for(i in 1:nsims){
-      #   segments( x0=f_hpdi[,order(f_med)[i]][1] , y0=seq_l[i] , 
-      #             x1=f_hpdi[,order(f_med)[i]][2], y1=seq_l[i] , col.alpha("darkgrey" , alpha=1) , lty=1 , lw=.5)
-      # }
+      for(i in 1:nsims){
+        segments( x0=f_hpdi[,order(f_med)[i]][1] , y0=seq_l[i] ,
+                  x1=f_hpdi[,order(f_med)[i]][2], y1=seq_l[i] , col.alpha("darkgrey" , alpha=1) , lty=1 , lw=.5)
+      }
       points(f_med[order(f_med)] , seq_l , cex=0.3 ,  col=mypalette[k_i] , alpha=0.99 , pch=5)
       #dens( zm[index,] , col=mypalette[k_i] , add=TRUE , lw=3) #post of all sims
       for (i in 1:nsims) dens(zm[index,i] , add=TRUE ,  col=col.alpha(mypalette[k_i] , alpha=0.15))
@@ -98,14 +98,14 @@ for (f_i in 1:length(F)){
       seq_l <- seq(from=0 , to=1.5 , length=nsims) #vertical range to plot hpdi segs
       #f_med_order <- order(apply(zm[index,] , 2 , median)) #order of model f medians by magnitude
       f_med <- apply(zm[index,] , 2 ,  median) #actual medians
-      f_hpdi <- apply(zm[index,] , 2 , HPDI) #actual medians
+      f_hpdi <- apply(zm[index,] , 2 , HPDI, prob=0.50) #actual medians
       for (i in 1:nsims) dens(zm[index,i] , add=TRUE ,  col=col.alpha(mypalette[k_i] , alpha=0.15))
       for(i in 1:nsims){
         segments( x0=f_hpdi[,order(f_med)[i]][1] , y0=seq_l[i] , 
                   x1=f_hpdi[,order(f_med)[i]][2], y1=seq_l[i] , col.alpha("darkgrey" , alpha=1) , lty=1 , lw=.5)
       }
       points(f_med[order(f_med)] , seq_l , cex=0.3 ,  col=mypalette[k_i] , alpha=0.99 , pch=5)
-      dens( zm[index,] , col=mypalette[k_i] , add=TRUE , lw=3) #post of all sims
+      dens( zm[index,] , col=mypalette[k_i] , add=TRUE , lw=3 , adj=2) #post of all sims
       axis(1, at=c(0,1,2,3,4,5,6,7,8) , cex.axis=0.8 , tck=0.01 , labels=FALSE )
       axis(1, at=c(0,1,2,3,4,5,6,7,8) , cex.axis=0.8 , tck=-0.01 , labels=FALSE )
       
@@ -158,7 +158,7 @@ for (f_i in 1:length(F)){
       seq_l <- seq(from=0 , to=1.5 , length=nsims) #vertical range to plot hpdi segs
       #f_med_order <- order(apply(zm[index,] , 2 , median)) #order of model f medians by magnitude
       f_med <- apply(zm[index,] , 2 ,  median) #actual medians
-      f_hpdi <- apply(zm[index,] , 2 , HPDI) #actual medians
+      f_hpdi <- apply(zm[index,] , 2 , HPDI, prob=0.50) #actual medians
       for (i in 1:nsims) dens(zm[index,i] , add=TRUE ,  col=col.alpha(mypalette[k_i] , alpha=0.15))
       for(i in 1:nsims){
         segments( x0=f_hpdi[,order(f_med)[i]][1] , y0=seq_l[i] , 
--- 02a_per pred plot.R
+++ 02a_per pred plot.R
@@ -1,43 +1,64 @@
-mydir = "/Users/sifaka/Downloads/rstudio-export-log"
+mydir = "/Users/sifaka/Downloads/rstudio-export-log/f_3"
 myfiles = list.files(path=mydir, pattern="*.csv", full.names=TRUE , recursive=TRUE)
 d = ldply(myfiles, read_csv)
+mypalette<-brewer.pal(max(K),"BuPu")
 
 d <- d[,-1] # get rid of bs first column
 nsims <- ncol(d) - 3 #number of sims 
-
+master2 <- d
 N <- sort(unique(d$n))  ## pop size vector from data
 F <-sort(unique(d$f))  ## strength of frequency dependence from data
 K <- sort(unique(d$k))  ## number of options from data
-mypalette<-brewer.pal(max(K),"Dark2")
-d2 <- d[d$n==20 & d$f==3 & d$k==2,]
-dens(d2[,4:53])
-dens(d2)
+# d2 <- d[d$n==20 & d$f==3 & d$k==2,]
+# dens(d2[,4:53])
+# dens(d2)
 
 dev.off()
 
 z2 <- master2[master2$k==2,] #subset relevant k
 z2m <- data.matrix(z2[,1:nsims]) #extract parameter estimates
-index <- which(z2$n==100) #get rows for relevant n in loop
-dens(z2m[index,], xlim=c(0,5))
+index <- which(z2$n==10) #get rows for relevant n in loop
+dens(z2m[index,], xlim=c(0,5) , col=mypalette[1] , adj=3 , ylim=c(0,1))
 
 z2 <- master2[master2$k==2,] #subset relevant k
 z2m <- data.matrix(z2[,1:nsims]) #extract parameter estimates
-index <- which(z2$n==100) #get rows for relevant n in loop
-dens(z2m[index,] , add=TRUE , col="black" )
+index <- which(z2$n==10) #get rows for relevant n in loop
+dens(z2m[index,] , add=TRUE , col=mypalette[1] , adj=3)
+HPDI(z2m[index,])
+median(z2m[index,])
+mean(z2m[index,])
+sum( z2m[index,] > 1 ) / (nsims*3000)
+chainmode( z2m[index,] , adj=0.01 )
+
 
 z3 <- master2[master2$k==3,] #subset relevant k
 z3m <- data.matrix(z3[,1:nsims]) #extract parameter estimates
-index <- which(z3$n==100) #get rows for relevant n in loop
-dens(z3m[index,] , add=TRUE , col="blue" )
+index <- which(z3$n==10) #get rows for relevant n in loop
+dens(z3m[index,] , add=TRUE , col=mypalette[2] , adj=3)
+HPDI(z3m[index,])
+median(z3m[index,])
+mean(z3m[index,])
+sum( z3m[index,] > 1 ) / (nsims*3000)
+chainmode( z3m[index,] , adj=0.01 )
 
 
 z4 <- master2[master2$k==4,] #subset relevant k
 z4m <- data.matrix(z4[,1:nsims]) #extract parameter estimates
-index <- which(z4$n==100) #get rows for relevant n in loop
-dens(z4m[index,] , add=TRUE , col="gold" )
+index <- which(z4$n==10) #get rows for relevant n in loop
+dens(z4m[index,] , add=TRUE , col=mypalette[3] , adj=3 )
+HPDI(z4m[index,])
+median(z4m[index,])
+mean(z4m[index,])
+sum( z4m[index,] > 1 ) / (nsims*3000)
+chainmode( z4m[index,] , adj=0.01 )
 
 
 z5 <- master2[master2$k==5,] #subset relevant k
 z5m <- data.matrix(z5[,1:nsims]) #extract parameter estimates
-index <- which(z5$n==100) #get rows for relevant n in loop
-dens(z5m[index,] , add=TRUE , col="red" )
+index <- which(z5$n==10) #get rows for relevant n in loop
+dens(z5m[index,] , add=TRUE , col=mypalette[4] , adj=3)
+HPDI(z5m[index,])
+median(z5m[index,])
+mean(z5m[index,])
+sum( z5m[index,] > 1 ) / (nsims*3000)
+chainmode( z5m[index,] , adj=0.01 )
