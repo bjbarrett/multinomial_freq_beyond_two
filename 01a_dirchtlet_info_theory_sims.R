9ddc54d0ecf9e0ec0f7f1c071b4a6c21d5e82e29
Brendan Barrett <bbarrett@ab.mpg.de>
2022-01-27 16:36:02 +0100

    started to work on grid approximation simulations

--- 01a_dirchtlet_info_theory_sims.R
+++ 01a_dirchtlet_info_theory_sims.R
@@ -3,11 +3,11 @@ library(gtools)
 library(FSelector)
 #lets simulate a vector of length K that sums to 1 to generate random frequencies.
 # We chose an alpha of one to cover a variety of prob combons
-
+K <- c(2,3,4,5)
 #x <- rdirichlet(n_sims*10000, alpha=rep(K,K[k_i]))
 mypalette<-brewer.pal(length(K),"Dark2")
 #lets plot where all the distributions are drawn form
-x <- rdirichlet(1e5, alpha=rep(1,K[1]) ,ylim=c(0,3))
+x <- rdirichlet(1e5, alpha=rep(1,K[1]))
 dens(x , col="white")
 for(i in 1:length(K)){
   x <- rdirichlet(1e5, alpha=rep(1.2,K[i]))
@@ -60,3 +60,36 @@ for(i in 1:length(K)){
   max <- apply( x , 1 , max)
   points(ent~max , col=mypalette[i] , cex=0.8 , pch=19)
 }
+
+### what we really want to do is for a given value of f with K=2,3, or 4 options count up all the ways that the observed data could occur
+### we also want to bumb up the sample size for a given value of f
+#1. generate observations of 2+3 traits
+#2. simulate data showing what they would choose given a particular strength of conformity
+#3. calculate the number of forking paths that would generate that observation
+#4. think carefully about how this is related to entropy
+q <- list()
+q$q2 <- c(0.5 , 0.5 )
+q$q3 <- c(1/3 , 1/3 , 1/3)
+q$q4 <- c(1/4 , 1/4 , 1/4 , 1/4)
+( H <- sapply( q , function(q) -sum(ifelse(q==0,0,q*log(q))) ) ) #calculate entropy
+zz <- rmultinom(n=1, size=1000, prob=c(0.9,0.1))
+zz/1000
+
+# 5 agents observe this (c(0.6 , 0.4))
+K <-2
+F <- 2.1
+N <- 20
+si_freq <- rdirichlet(1, alpha=rep(1,K))
+si_freq <- c(0.45,0.55)
+s_temp <- si_freq[1:K]^F
+pr_choose <- s_temp/sum(s_temp)
+choices <- sample( 1:K , size=N , prob=pr_choose , replace=TRUE) #what are all the values of F that could give rise to this data?
+
+
+p_grid <- seq( from=0 , to=1 , length.out=1000 )
+prior <- rep(1,1000)
+likelihood <- dbinom( 3 , size=3 , prob=p_grid )
+posterior <- likelihood * prior
+posterior <- posterior / sum(posterior)
+samples <- sample( p_grid , size=1e4 , replace=TRUE , prob=posterior )
+dens(samples)
